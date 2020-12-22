541fe3e1d876f53a2376dda4d3c471b4a9c350d8
Marcin <zasadzkimarcin@gmail.com>
2020-12-22 20:47:38 +0000

    fix issue with running multiple files at once

--- index.js
+++ index.js
@@ -1,26 +1,3 @@
-const {getAudio} = require('./src/exportAudio');
-const {getFilesListArray, replaceExtensionToMp3} = require('./src/manageFiles');
+const {extract} = require('./src/extract')
 
-const [inputArg, outputArg] = process.argv.slice(2);
-
-const INPUT_DIR = './input'
-const OUTPUT_DIR = './output'
-
-const inputPath = inputArg || INPUT_DIR
-const outputPath = outputArg || OUTPUT_DIR
-
-const filesList = getFilesListArray(inputPath);
-
-const amountOfFiles = filesList.length;
-
-if(amountOfFiles){
-  for(let i = 0; i < amountOfFiles; i++){
-    const inputFile = `${inputPath}/${filesList[i]}`
-    const outputFileName = replaceExtensionToMp3(filesList[i])
-    const outputFile = `${outputPath}/${outputFileName}`
-  
-    getAudio(inputFile, outputFile)
-  }
-} else {
-  console.log('No files in the directory')
-}
+extract()
--- src/exportAudio.js
+++ src/exportAudio.js
@@ -2,15 +2,24 @@ const ffmpeg = require('fluent-ffmpeg')
 
 const MP3_CODEC = 'libmp3lame'
 
-function getAudio(input, output){
-  ffmpeg(input)
+async function getAudio(input, output){
+  return new Promise((res, rej) => {
+    ffmpeg(input)
     .output(output)
     .noVideo()
     .audioCodec(MP3_CODEC)
     .on('start', () => console.log('Processing:', input))
-    .on('end', () => console.log('Finished:', output))
-    .on('error', (err) => console.log('Error happened: ', err))
+    .on('end', () => {
+      console.log('Finished:', output)
+      res()
+    })
+    .on('error', (err) => {
+      console.log('Error happened: ', err)
+      rej()
+    })
     .run()
+  })
+  
 }
 
 module.exports = {
